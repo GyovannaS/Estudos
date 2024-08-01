33677b1961e7db799f8ebde89165a2b47dc2e320
GyovannaS <116318999+GyovannaS@users.noreply.github.com>
2024-07-31 21:25:14 -0300

    Create calcularMedia.java

---
+++ calcularMedia.java
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado</title>
+</head>
+<body>
+    <h1>Resultado da Média</h1>
+    <% 
+        double nota1 = Double.parseDouble(request.getParameter("nota1"));
+        double nota2 = Double.parseDouble(request.getParameter("nota2"));
+        double media = (nota1 + nota2) / 2;
+    %>
+    <p>A média das notas <%= nota1 %> e <%= nota2 %> é: <%= media %></p>
+</body>
+</html>
